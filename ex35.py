adc55c4cfb98f592b97ab584566aaceb3e9527fe
isabellyfd <isabellyfd@hotmail.com>
2015-10-01 16:18:39 -0300

    39 done

--- ex35.py
+++ ex35.py
@@ -22,7 +22,7 @@ def bear_room():
 	print "the fat bear is in front of another door"
 	print "how are you goin to move the bear?"
 
-	bear_moved = false
+	bear_moved = False
 
 	while True:
 		choice = raw_input("> ")
@@ -32,4 +32,44 @@ def bear_room():
 		elif choice == "taunt bear" and not bear_moved:
 			print "the bear has moved from the door. you can go throught it"
 			bear_moved = True
+		elif choice == "taunt bear"  and bear_moved:
+			dead("The bear get pissed off and chews your leg off")
+		elif choice == "open door" and bear_moved:
+			gold_room()
+		else:
+			print "I got no ideia what that means"
+
+def cthulhu_room():
+	print "here you see the great evil Cthulhu"
+	print "he, it, whatever stares at you and you go insane"
+	print "do you flee for your life or eat your head?"
+
+	choice = raw_input("> ")
+	
+	if "flee" in choice:
+		start()
+	elif "head" in choice:
+		dead("well that was tasty!")
+	else:
+		cthulhu_room()
+
+def dead (why):
+	print why, "good job!"
+	exit(0)
+
+
+def start():
+	print "You ar in a dark room"
+	print "there is a door to your right and left"
+	print "which one you take?"
+
+	choice = raw_input("> ")
 		
+	if choice == "left":
+		bear_room()
+	elif choice ==  "right":
+		cthulhu_room()
+	else: dead("you stumble around the room until starve")
+
+
+start()
